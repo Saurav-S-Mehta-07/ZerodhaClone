4ae12625d9744138f132189a93434a6f7407c6af
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-28 15:57:40 +0530

    mongo atlas added

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 
 const { HoldingsModel } = require("./models/HoldingsModel");
 const { PositionsModel } = require("./models/PositionsModel");
+const { OrdersModel } = require("./models/OrdersModel");
 
 const PORT = process.env.PORT || 8080;
 const Mongo_URL = process.env.MONGO_URL;
@@ -206,6 +207,19 @@ app.get("/allPositions",async(req,res)=>{
     let allPositions = await PositionsModel.find({});
     res.json(allPositions);
 });
+
+app.post("/newOrder",async(req,res)=>{
+        let newOrder = new OrdersModel({
+             name: req.body.name,
+             qty: req.body.qty,
+             price: req.body.price,
+             mode: req.body.mode,
+        });
+
+        newOrder.save();
+
+        res.send("Order saved");
+})
 
 app.listen(PORT, () => {
   console.log("App started");
